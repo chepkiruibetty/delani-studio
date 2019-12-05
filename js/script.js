8f19902e9948dfbfa2607d8db639c99912167685
chepkiruibetty <chepkiruibetty64@gmail.com>
2019-12-05 16:39:40 +0300

    write a jquery for product development

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
  $("#dotext").hide(); 
- $("#dotext2").hide(); 
+ $("#dotext2").hide();
+ $("#dotext3").hide(); 
 $(".imagetoggle0 , .imagetoggle1").click(function(){
 $("#dotext").toggle();
 $(".imagetoggle0").toggle();
@@ -8,6 +9,10 @@ $(".imagetoggle0").toggle();
 $(".imagetoggle2 , .imagetoggle3").click(function(){
 $("#dotext2").toggle();
 $("#imagetoggle2").toggle();
+});
+$(".imagetoggle4 , .imagetoggle5").click(function(){
+$("#dotext3").toggle();
+$(".imagetoggle4").toggle();    
 });
 
 });
